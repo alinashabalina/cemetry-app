2fd3861b526271d45ff69d2ab85d38d64353b5a3
Alina Shabalina <alinavshabalina@gmail.com>
2024-06-07 13:07:00 +0200

    fix seeds and links

--- app/controllers/graveyards_controller.rb
+++ app/controllers/graveyards_controller.rb
@@ -17,6 +17,9 @@ class GraveyardsController < ApplicationController
       lng: @graveyard.longitude,
       info_window_html: render_to_string(partial: "info_window", locals: {graveyard: @graveyard})
     }]
+
+    @tours = Tour.where("graveyard_id":params[:id])
+    GraveyardsChannel.broadcast_to(@graveyard, @tours)
 end
 
 def info
--- app/javascript/controllers/graveyard_subscription_controller.js
+++ app/javascript/controllers/graveyard_subscription_controller.js
@@ -3,12 +3,17 @@ import { createConsumer } from "@rails/actioncable"
 
 export default class extends Controller {
   static targets = ["info"]
-  static values = {id: { type: String, default: window.location.pathname.split('/')[4]}}
+  static values = {id: { type: String, default: window.location.pathname.split('/')[4]}, city: { type: String, default: window.location.pathname.split('/')[2]}}
 
   connect() {
       this.subscription = createConsumer().subscriptions.create(
         { channel: "GraveyardsChannel", id: this.idValue},
-        { received: data => {data.forEach((el) => this.infoTarget.textContent = el.title);}})}
+        { received: data => {if (data.length > 0) {data.forEach((el) => this.infoTarget.textContent = el.title);} else {this.infoTarget.textContent ="No upcoming tours"}}})}
+
+    refresh() {
+      console.log(window.location.pathname.split('/')[2])
+      fetch(`/cities/${this.cityValue}/graveyards/${this.idValue}`)
+    }
 
     disconnect() {
       this.subscription.unsubscribe()
