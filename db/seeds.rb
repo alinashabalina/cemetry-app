bbfa9d4fec7db9fd7e90410519c1c5f8010dfa1c
Alina Shabalina <alinavshabalina@gmail.com>
2024-06-07 12:14:44 +0200

    fix styles

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,11 @@ city = City.new("title": "Bad Schandau", "country": "Germany")
 city.save!
 city = City.new("title": "Berlin", "country": "Germany")
 city.save!
+city = City.new("title": "Dresden", "country": "Germany")
+city.save!
 gry = Graveyard.new("title": "Dreifaltigkeitskirchhof", "city_id": 2, "details": "In der ehemaligen Friedhofskapelle des Friedhofs befindet sich seit Anfang November 2013 eine Dauerausstellung über die Familie Mendelssohn.", "address": "Berlin, Hallesches Tor")
 gry.save!
 gry = Graveyard.new("title": "Friedhof Bad Schandau", "city_id": 1, "details": "Bad Schandau cemetery contains in total 115 German war graves from World War II.", "address": "01814, Bad Schandau")
+gry.save!
+gry = Graveyard.new("title": "Innerer Matthäusfriedhof", "city_id": 3, "details": "Der Innere Matthäusfriedhof ist einer von zwei Matthäusfriedhöfen der Stadt Dresden. Er liegt in der Friedrichstadt in unmittelbarer Nähe zur Matthäuskirche und gehört zu den kleinen Friedhöfen der Stadt.", "address": " 01067, Dresden")
 gry.save!
